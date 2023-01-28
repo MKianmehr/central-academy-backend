08ddaa2a4de69b0db3d69ae4b74993c6dcf885df
Mohammadreza Kianmehr <mohammad.r.kianmehr@gmail.com>
2023-01-28 08:24:34 +0330

    Feat: login in

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,9 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { CreateUserDto } from './dtos/create_user.dto';
 import { User } from './user.schema';
+import { LoginUserDto } from './dtos/login_user.dto';
+import * as bcrypt from 'bcrypt';
 
 
 @Injectable()
@@ -10,11 +12,19 @@ export class AuthService {
 
     async signUp(createUserDto: CreateUserDto): Promise<User> {
         const user = await this.usersService.findOne(createUserDto.email)
-        if (user) throw new BadRequestException('email in use')
+        if (user) throw new ConflictException('email in use')
 
         const newUser = await this.usersService.create(createUserDto)
         return newUser
     }
 
-    async signIn() { }
+    async signIn(loginUserDto: LoginUserDto): Promise<User> {
+        const { email, password } = loginUserDto
+        const user = await this.usersService.findOne(email)
+
+        if (user && await bcrypt.compare(password, user.password)) {
+            return user
+        }
+        throw new UnauthorizedException('Please check your login credentials')
+    }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,21 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { CreateUserDto } from "./dtos/create_user.dto";
-import { User } from "./user.schema";
+import { LoginUserDto } from "./dtos/login_user.dto";
 
 @Controller('auth')
 export class UsersController {
     constructor(private authService: AuthService) { }
 
     @Post('/signup')
-    async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    async createUser(@Body() createUserDto: CreateUserDto) {
         const user = await this.authService.signUp(createUserDto)
+        return user
+    }
+
+    @Post('/signin')
+    async loginUser(@Body() loginUserDto: LoginUserDto) {
+        const user = await this.authService.signIn(loginUserDto)
         return user
     }
 }
