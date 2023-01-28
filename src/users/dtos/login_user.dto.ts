114b9a1ca0b7c929fe9dd931ec596158d31f3c6b
Mohammadreza Kianmehr <mohammad.r.kianmehr@gmail.com>
2023-01-28 08:43:30 +0330

    Feat: add Swagger

--- src/users/dtos/login_user.dto.ts
+++ src/users/dtos/login_user.dto.ts
@@ -1,11 +1,14 @@
 import { IsEmail, IsString, MinLength } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginUserDto {
 
+    @ApiProperty()
     @IsEmail()
     @IsString()
     email: string;
 
+    @ApiProperty({ minimum: 6 })
     @MinLength(6)
     @IsString()
     password: string;
