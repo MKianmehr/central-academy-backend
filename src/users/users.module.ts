178dc930db7a91ada2f973b09749c4372a3b477f
Mohammadreza Kianmehr <mohammad.r.kianmehr@gmail.com>
2023-02-03 13:04:34 +0330

    Fix: mongo throws error because of env variable

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,24 +5,30 @@ import { User, UserSchema } from './user.schema';
 import { UsersController } from './users.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
-import { AdminGuard } from './guards/admin.guard';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
-  imports: [MongooseModule.forFeature([
-    {
-      name: User.name,
-      schema: UserSchema
-    }
-  ]),
-  JwtModule.register({
-    secret: process.env.JWT_KEY,
-    signOptions: {
-      expiresIn: 86400,
-    }
-  })
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema
+      }
+    ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>("JWT_KEY"),
+        signOptions: {
+          expiresIn: 86400,
+        }
+      }),
+      inject: [ConfigService]
+    })
   ],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, AdminGuard],
-  exports: []
+  providers: [UsersService, AuthService],
 })
 export class UsersModule { }
+
