ed76b02e06274ee1ae0b4e40ca6efd8a188642a9
Mohammadreza Kianmehr <mohammad.r.kianmehr@gmail.com>
2023-02-03 13:01:16 +0330

    Fix: mongo throws error because of env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,9 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URL),
     UsersModule,
-    ConfigModule.forRoot({ isGlobal: true })
   ],
 })
 export class AppModule { }
